1586660a5093d15c1ff639860540fd109c8111f2
yuwalgill <75741949+yuwalgill@users.noreply.github.com>
2021-01-27 13:31:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var car,wall,speed,weight,deformation;
+
+ function setup() {
+  createCanvas(1600,400);
+  car=createSprite(50, 50, 50,30);
+  
+wall=createSprite(1200,50,10,height);
+speed=random(55,90);
+  weight=random(400,1500);
+car.velocityX=speed
+
+}
+
+function draw() {
+  background(80,80,80);  
+  
+ 
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0;
+    deformation=0.5*weight*speed*speed/22500    
+   if(deformation>180){
+    car.shapeColor=shapeColor(255,0,0)
+   }
+  if(deformation<180&&deformation>100){
+    car.shapeColor=color(230,230,0);
+  }
+  if(deformation<100){
+    car.shapeColor=color(0,255,0)
+  }
+ 
+
+}
+
+drawSprites();
+}
